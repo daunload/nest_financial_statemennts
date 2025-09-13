040a563b73af37066fa8838f1ce62abb95734d00
daunload <daun4799@gmail.com>
2025-09-13 09:57:55 +0900

    지표 api 추가

--- src/financials/financials.controller.ts
+++ src/financials/financials.controller.ts
@@ -52,4 +52,41 @@ export class FinancialsController {
 
 		return response.results;
 	}
+	@Get('/macd')
+	async getMACD(@Query('ticker') ticker: string) {
+		const response = await this.financialsService.getMACD({
+			stockTicker: ticker,
+			timespan: 'day',
+			expand_underlying: true,
+			order: 'desc',
+			limit: 40,
+		});
+
+		return response.results;
+	}
+	@Get('/ema')
+	async getEMA(@Query('ticker') ticker: string) {
+		const response = await this.financialsService.getEMA({
+			stockTicker: ticker,
+			timespan: 'day',
+			expand_underlying: true,
+			order: 'desc',
+			limit: 40,
+		});
+
+		return response.results;
+	}
+	@Get('/rsi')
+	async getRSI(@Query('ticker') ticker: string) {
+		const response = await this.financialsService.getRSI({
+			stockTicker: ticker,
+			timespan: 'day',
+			expand_underlying: true,
+			window: 14,
+			order: 'desc',
+			limit: 40,
+		});
+
+		return response.results;
+	}
 }
